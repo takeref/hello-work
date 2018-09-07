f1a02578220b56b88913aa3394cc4d4365f0462c
Take <Take@0000O0000A0001>
2018-09-06 23:56:37 -0500

    hola mundo

---
+++ src/HelloWork.java
@@ -0,0 +1,9 @@
+//declaracion de la clase 
+public class HelloWork {
+	//inicia el metodo principal desde donde comenzara a correr nuestro programa
+	public static void main(String Argumetos[]) 
+	{
+		System.out.print("Bienvenido al mundo de la programacion en JAVA");
+	} //fin del metodo main
+
+}//fin de la clase 
