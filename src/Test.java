379169284afc61c68ba8a155ed85735efa65619c
fer <fer@THINKLNX>
2018-09-08 13:08:20 -0500

    Pruba de update desde la LENOVO...

--- src/Test.java
+++ src/Test.java
@@ -3,8 +3,8 @@ public class Test {
 	
 	public static void main(String Argumentos[]) 
 	{
-		System.out.print("este es un mesaje de prueb en la consola de JAVA");
-		System.out.print("un update mas a este jale");
+		System.out.println("este es un mesaje de prueb en la consola de JAVA");
+		System.out.println("un update mas a este jale");
+		System.out.println("Prueba de update desde LENOVO THINLPAD");
 	}
-
-}
+}	
